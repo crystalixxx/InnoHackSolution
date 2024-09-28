839aafa4fa75d201990bd1bfb42ed7758a046df7
Maxim Drugov <midrugov@edu.hse.ru>
2024-09-28 12:39:36 +0300

    scripts

---
+++ scripts/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Exit in case of error
+set -e
+
+# Build and run containers
+docker compose up
+
+# Hack to wait for postgres container to be up before running alembic migrations
+sleep 5;
